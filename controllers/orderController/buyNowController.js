696e51a54c0e60916dd57e07e71160beb9312eaa
vansh432 <vanshdeep703@gmail.com>
2025-03-02 18:30:15 +0530

    changes done

--- controllers/orderController/buyNowController.js
+++ controllers/orderController/buyNowController.js
@@ -3,16 +3,16 @@ import pool from "../../config/db.js"
 
 export const buyOrders = async (req, res) => {
     try {
-        const { coin, color, products, size, product_configuration_id, old_price, price, quantity, product_name, user_id, image_url } = req.body
-        if (!req?.file && !coin && !product_configuration_id && !color && !products && !product_name && !old_price && !price && !user_id && !image_url) {
+        const { coin, color, products, size, product_configuration_id, old_price, price, quantity, product_name, user_id, image_url, vendor_id } = req.body
+        if (!req?.file && !coin && !product_configuration_id && !color && !products && !product_name && !old_price && !price && !user_id && !image_url && !vendor_id) {
             return res.status(400).json({
                 status: "error",
                 message: "Missing required fields"
             })
         }
 
-        const queryBuyOrder = `INSERT INTO buyorder (user_id,coin,color,products,size,product_configuration_id,old_price,price,quantity,product_name,image_url) VALUES (?,?,?,?,?,?,?,?,?,?,?)`
-        const values = [user_id, coin, color, products, size, product_configuration_id, old_price, price, quantity, product_name, image_url]
+        const queryBuyOrder = `INSERT INTO buyorder (user_id,coin,color,products,size,product_configuration_id,old_price,price,quantity,product_name,image_url,vendor_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`
+        const values = [user_id, coin, color, products, size, product_configuration_id, old_price, price, quantity, product_name, image_url, vendor_id]
 
         const dataBuyOrder = await queryPromis(queryBuyOrder, values);
         if (!dataBuyOrder) return res.status(400).json({
@@ -69,33 +69,33 @@ export const getBuyOrder = async (req, res) => {
 
 
 export const cancelOrder = async (req, res) => {
-    try{
-        const {orderId}=req.params;
-        if(!orderId){
+    try {
+        const { orderId } = req.params;
+        if (!orderId) {
             return res.status(200).json({
-                status:"error",
-                message:"Missing required fields"
+                status: "error",
+                message: "Missing required fields"
             })
         }
-        const queryCancelOrder=`UPDATE order_items  SET status='canceled' WHERE id=?`;
-        const value=[orderId];
-        const cancelOrder=await queryPromis(queryCancelOrder,value);
-        if(!cancelOrder){
+        const queryCancelOrder = `UPDATE order_items  SET status='canceled' WHERE id=?`;
+        const value = [orderId];
+        const cancelOrder = await queryPromis(queryCancelOrder, value);
+        if (!cancelOrder) {
             return res.status(404).json({
-                status:"error",
-                message:"No order found"
+                status: "error",
+                message: "No order found"
             })
         }
         return res.status(200).json({
-            status:"success",
-            message:"Order is canceled successfully",
-            data:cancelOrder
+            status: "success",
+            message: "Order is canceled successfully",
+            data: cancelOrder
         })
-    }catch(err){
+    } catch (err) {
         return res.status(500).json({
-            status:"error",
-            message:"Operation failed",
-            error:err.message
+            status: "error",
+            message: "Operation failed",
+            error: err.message
         })
     }
 }
@@ -153,46 +153,46 @@ export const orderItems = async (req, res) => {
             order_items,
             total_coins
         } = req.body;
-
-        if (!req?.file &&!total_items &&!payment_type &&!total_amount &&!net_amount &&!user_id &&!shipping_charges &&!shipping_address_id &&!order_items &&  !total_coins) {
+         console.log(req.file.filename)
+        if (!req?.file && !total_items && !payment_type && !total_amount && !net_amount && !user_id && !shipping_charges && !shipping_address_id && !order_items && !total_coins && !req.file.filename) {
             return res.status(400).json({
                 status: "error",
                 message: "Missing required fields"
             })
         }
-     
-            const queryAddCoins=`INSERT INTO orders_cart (total_items,payment_type,total_amount,net_amount,user_id,shipping_charges,shipping_address_id,total_coins)  VALUES (?, ?, ?, ?, ?, ?, ?,?)`
-            const valuesAddCoins=[total_items, payment_type, total_amount, net_amount, user_id, shipping_charges, shipping_address_id, total_coins]
-            const addCoinsDone= await queryPromis(queryAddCoins,valuesAddCoins);
-            if(!addCoinsDone) return res.status(400).json({
-                status: "error",
-                message: "Failed to add coins"
-            })
-            for (const item of order_items) {
-                const queryAddItems=`INSERT INTO order_items (order_id, product_id, size, color, sales_price, old_price, vendor_id, total_price, quantity,coins) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)`
-                const valuesAddItems=[addCoinsDone.insertId, item.product_id, item.size, item.color, item.sales_price, item.old_price, item.vendor_id, item.total_price, item.quantity,item.coins]
-                await queryPromis(queryAddItems, valuesAddItems);
-            }
-            const queryCoinHistory=`INSERT INTO coins_history (heading,coin_add_at,coin,user_id,coinStatus,orderId) VALUES (?,?,?,?,?,?)`
 
-            const valueCoinHistory=["product A",new Date(),total_coins,user_id,true,addCoinsDone.insertId || null];
-            const queryCoinHistoryAdded=await queryPromis(queryCoinHistory,valueCoinHistory);
-            if(!queryCoinHistoryAdded){
-                return res.status(400).json({
-                    status: "error",
-                    message: "Failed to add coins history"
-                })
-            }
-   
-            const queryAddCoinsUser = `UPDATE coins SET value=value+? WHERE user_id=?`;
-            const valueAddcoin=[total_coins,user_id]
-            await queryPromis(queryAddCoinsUser,valueAddcoin);
-        
-           return res.status(200).json({
+        const queryAddCoins = `INSERT INTO orders_cart (total_items,payment_type,total_amount,net_amount,user_id,shipping_charges,shipping_address_id,total_coins)  VALUES (?, ?, ?, ?, ?, ?, ?,?)`
+        const valuesAddCoins = [total_items, payment_type, total_amount, net_amount, user_id, shipping_charges, shipping_address_id, total_coins]
+        const addCoinsDone = await queryPromis(queryAddCoins, valuesAddCoins);
+        if (!addCoinsDone) return res.status(400).json({
+            status: "error",
+            message: "Failed to add coins"
+        })
+        for (const item of order_items) {
+            const queryAddItems = `INSERT INTO order_items (order_id, product_id, size, color, sales_price, old_price, vendor_id, total_price, quantity,coins,product_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)`
+            const valuesAddItems = [addCoinsDone.insertId, item.product_id, item.size, item.color, item.sales_price, item.old_price, item.vendor_id, item.total_price, item.quantity, item.coins,req.file.filename]
+            await queryPromis(queryAddItems, valuesAddItems);
+        }
+        const queryCoinHistory = `INSERT INTO coins_history (heading,coin_add_at,coin,user_id,coinStatus,orderId) VALUES (?,?,?,?,?,?)`
+
+        const valueCoinHistory = ["product A", new Date(), total_coins, user_id, true, addCoinsDone.insertId || null];
+        const queryCoinHistoryAdded = await queryPromis(queryCoinHistory, valueCoinHistory);
+        if (!queryCoinHistoryAdded) {
+            return res.status(400).json({
+                status: "error",
+                message: "Failed to add coins history"
+            })
+        }
+
+        const queryAddCoinsUser = `UPDATE coins SET value=value+? WHERE user_id=?`;
+        const valueAddcoin = [total_coins, user_id]
+        await queryPromis(queryAddCoinsUser, valueAddcoin);
+
+        return res.status(200).json({
             status: "success",
             message: "order items add successfully"
-           })
-        
+        })
+
 
     } catch (err) {
         return res.status(500).json({
@@ -216,23 +216,48 @@ const queryPromis = (query, value = []) => {
 }
 
 
-export const getAllOrders=(req,res)=>{
-    try{
+export const getAllOrders = async (req, res) => {
+    try {
 
-    const {userId}=req.params;
-    const {orderStatus}=req.body;
-    if(orderStatus==="all"){
-    const queryAllOrders=`SELECT * FROM orders_cart WHERE user_id=?`;
-    }else {
+        const { userId } = req.params;
+        if (!userId) {
+            return res.status(400).json({
+                status: "error",
+                message: "Missing required fields"
+            })
 
-    }
-    const value=[userId];
-
-    }catch(err){
+        }
+        const queryAllOrders = `SELECT * FROM orders_cart WHERE user_id=?`;
+        const values = [userId];
+        const allOrders = await queryPromis(queryAllOrders, values);
+        const arrayOrders = [];
+        for (let key in allOrders) {
+            const queryOrderItems = `SELECT * FROM order_items WHERE order_id=?`
+            const valueOrder = [allOrders[key]?.id];
+            const orderItems = await queryPromis(queryOrderItems, valueOrder);
+            if (orderItems[0].length != 0) {
+                const productName=await queryPromis(`SELECT name FROM products WHERE id=?`,[orderItems[0].product_id]);
+             
+                orderItems[0].product_name=productName[0]?.name;
+                arrayOrders.push(orderItems[0])
+            }
+        }
+        if (arrayOrders.length == 0) {
+            return res.status(404).json({
+                status: "error",
+                message: "No orders found"
+            })
+        }
+        return res.status(200).json({
+            status: "success",
+            message: "All orders fetched successfully",
+            data: arrayOrders
+        })
+    } catch (err) {
         return res.status(500).json({
-            status:"error",
-            message:"Operation failed",
-            error:err.message
+            status: "error",
+            message: "Operation failed",
+            error: err.message
         })
     }
 }
--- routes/order.js
+++ routes/order.js
@@ -1,16 +1,26 @@
 import {Router} from 'express';
-import {buyOrders,getBuyOrder,orderItems,cancelOrder} from '../controllers/orderController/buyNowController.js'
+import {buyOrders,getBuyOrder,orderItems,cancelOrder,getAllOrders} from '../controllers/orderController/buyNowController.js'
 import multer from 'multer';
 const routes=Router();
 
+const storage=multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/orders/');
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+})
 
+const upload=multer({ storage });
 
 
 routes.post('/buyOrders',buyOrders)
 routes.get('/buyOrders/:buyId',getBuyOrder)
-.post('/orderItems',orderItems)
+.post('/orderItems',upload.single('product_image'),orderItems)
 .put('/:orderId',cancelOrder)
-.get('/getAllOrders/:userId',)
+.get('/getAllOrders/:userId',getAllOrders)
 
 
 export default routes
