5f5a48e7d6bfef9517f730bae7e71243100a1a3f
vansh432 <vanshdeep703@gmail.com>
2025-03-02 18:52:28 +0530

    order placed api

--- routes/order.js
+++ routes/order.js
@@ -3,22 +3,11 @@ import {buyOrders,getBuyOrder,orderItems,cancelOrder,getAllOrders} from '../cont
 import multer from 'multer';
 const routes=Router();
 
-const storage=multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/orders/');
-    },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + '-' + file.originalname);
-    }
-})
-
-const upload=multer({ storage });
 
 
 routes.post('/buyOrders',buyOrders)
 routes.get('/buyOrders/:buyId',getBuyOrder)
-.post('/orderItems',upload.single('product_image'),orderItems)
+.post('/orderItems',orderItems)
 .put('/:orderId',cancelOrder)
 .get('/getAllOrders/:userId',getAllOrders)
 
