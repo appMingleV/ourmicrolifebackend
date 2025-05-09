58b0b70f049b00c24d70858960ddbc560e0129d2
vansh432 <vanshdeep703@gmail.com>
2025-05-09 06:35:03 +0530

    server improvement

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ dotenv.config();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
-app.use("/uploads", express.static("uploads"));
-app.use("/api", route);
 
-app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api", route);
 
